c2f3b19fdfc1d1676036471f85cbcb97fd55bb42
Filipe Catarino <phelpsyt@gmail.com>
2024-07-19 15:43:10 +0100

    First Commit - Testing

---
+++ src/client/kotlin/github/phelpsyt/dynamicmovement/DynamicMovementClient.kt
@@ -0,0 +1,9 @@
+package github.phelpsyt.dynamicmovement
+
+import net.fabricmc.api.ClientModInitializer
+
+object DynamicMovementClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/github/phelpsyt/dynamicmovement/DynamicMovementDataGenerator.kt
@@ -0,0 +1,9 @@
+package github.phelpsyt.dynamicmovement
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object DynamicMovementDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
